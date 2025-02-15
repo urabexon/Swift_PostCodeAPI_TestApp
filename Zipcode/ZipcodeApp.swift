8513e330280161db35f1701b49138b8c81b2287d
urabexon <hiroki708.a48uk@gmail.com>
2025-02-15 13:47:47 +0900

    Initial Commit

---
+++ Zipcode/ZipcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZipcodeApp.swift
+//  Zipcode
+//
+//  Created by 卜部大輝 on 2025/02/15.
+//
+
+import SwiftUI
+
+@main
+struct ZipcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ZipcodeTests/ZipcodeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ZipcodeTests.swift
+//  ZipcodeTests
+//
+//  Created by 卜部大輝 on 2025/02/15.
+//
+
+import Testing
+@testable import Zipcode
+
+struct ZipcodeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
